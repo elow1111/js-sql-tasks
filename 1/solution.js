5fcdb5ece938d568b5b87c65fb5d6d1c329d229f
Максим <160880502+elow1111@users.noreply.github.com>
2024-11-08 19:34:27 +0300

    solution.js

--- 1/solution.js
+++ 1/solution.js
@@ -8,5 +8,19 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+const sql = postgres(config);
 
+export default async function solution() {
+  await sql`
+        CREATE TABLE IF NOT EXISTS articles (
+            title VARCHAR(255),
+            description VARCHAR(255)
+        );
+    `;
+
+  await sql`
+        INSERT INTO articles (title, description)
+        VALUES ('HAHAHHAHAHAHAHHA', 'HAHAHAHHAHAHAHAHAHHAHAHAHAHHAHAHAHAHAHHAHAHA');
+    `;
+}
 // END
