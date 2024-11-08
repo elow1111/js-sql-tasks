7dfa62d09ca6d807e93e1a144be873be0f811745
Максим <160880502+elow1111@users.noreply.github.com>
2024-11-08 19:45:55 +0300

    solution.js

--- 4/solution.js
+++ 4/solution.js
@@ -8,5 +8,44 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+const sql = postgres(config);
 
+export default async (client, user, roomNumber, price) => {
+  const { username, phone } = user;
+
+  const transaction = await client.transaction();
+
+  try {
+    const [newUser] = await transaction`
+      INSERT INTO users (username, phone)
+      VALUES (${username}, ${phone})
+      RETURNING id
+    `;
+
+    const [room] = await transaction`
+      SELECT id FROM rooms WHERE room_number = ${roomNumber} AND status = 'free'
+    `;
+
+    if (!room) {
+      throw new Error('Room nedost');
+    }
+
+    await transaction`
+      INSERT INTO orders (user_id, room_id, price)
+      VALUES (${newUser.id}, ${room.id}, ${price})
+    `;
+
+    await transaction`
+      UPDATE rooms
+      SET status = 'reserved'
+      WHERE id = ${room.id}
+    `;
+
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    console.error("Error dumai lol:", error);
+    throw error;
+  }
+};
 // END
