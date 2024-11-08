fea0b290897fd3a0ed1db42430eaf513077e8610
Максим <160880502+elow1111@users.noreply.github.com>
2024-11-08 19:37:01 +0300

    solution.js

--- 2/solution.js
+++ 2/solution.js
@@ -8,5 +8,17 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+const sql = postgres(config);
 
+export default async function solution(articles) {
+  const data = articles.map(({ title, description }) => [title, description]);
+  const result = await sql.begin(async (tx) => {
+    return await tx`
+      INSERT INTO articles (title, description)
+      VALUES ${sql(data)}
+      RETURNING id
+    `;
+  });
+
+  return result.map(row => row.id);
 // END
